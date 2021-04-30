2d642ee78da8ef9715125c55e001da5043daacbd
yash <ymehta777@gmail.com>
2021-04-30 13:41:33 +0530

    :zap: Itegrate nextJS and tailwindCSS, Make Header and Layout components. Also create a campaign via remix for frotend to display

---
+++ ethereum/web3.js
@@ -0,0 +1,10 @@
+import Web3 from "web3";
+let web3;
+if (typeof window !== "undefined") {
+  web3 = new Web3(window.ethereum);
+} else {
+  web3 = new Web3(
+    "https://rinkeby.infura.io/v3/98667df842b643c5a68077377ac327a2"
+  );
+}
+export default web3;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,46 @@
-const Home = () => {
-  return (
-    <>
-      <h1>hello world</h1>
-    </>
-  );
-};
+import React from "react";
+import factory from "../ethereum/factory";
+import web3 from "../ethereum/web3";
+import Layout from "../components/layout";
+import Link from "next/link";
+
+export async function getStaticProps() {
+  const accounts = await web3.eth.getAccounts();
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+  return {
+    props: {
+      campaigns,
+    },
+  };
+}
+
+class Home extends React.Component {
+  renderCampaigns = () => {
+    return this.props.campaigns.map((addr) => {
+      return (
+        <div className="mt-3 p-3 flex justify-between shadow rounded items-center bg-gray-50">
+          <div className="flex flex-col ml-3">
+            <p>Description</p>
+            <p>Campaign address {addr}</p>
+          </div>
+          <button className="py-0 px-2 bg-purple-600 text-white rounded h-9 mr-3 shadow">
+            View Details
+          </button>
+        </div>
+      );
+    });
+  };
+  render() {
+    return (
+      <div className="h-screen bg-gray-100">
+        <Layout>
+          <div className="p-5">
+            <p className="text-lg font-serif">Running Campaigns</p>
+            {this.renderCampaigns()}
+          </div>
+        </Layout>
+      </div>
+    );
+  }
+}
 export default Home;
