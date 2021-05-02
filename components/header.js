119a7b5587917ed37578a93ab3887bf3fb5a2510
yash <ymehta777@gmail.com>
2021-05-02 14:21:54 +0530

    :fire: Add name to the navbar brand

--- components/header.js
+++ components/header.js
@@ -12,12 +12,15 @@ const Header = () => {
         ></script>
       </Head>
       <div id="navbar" className="p-4 bg-gray-50 shadow">
-        <div className="flex mx-auto justify-between w-48 items-center">
+        <div className="flex mx-auto justify-between w-72 items-center">
           <i aria-hidden className="fas fa-users fa-2x text-purple-600"></i>
           <Link href="/">
             <a>
               <h2 className="text-3xl font-display text-purple-600">
-                CrowdCoin
+                CrowdCoin{" "}
+                <span className="text-sm text-gray-700 font-medium">
+                  by Yash Mehta
+                </span>
               </h2>
             </a>
           </Link>
