c64b7fb786e902235121526abb8f8ae53f312cc8
yash <ymehta777@gmail.com>
2021-05-02 13:35:50 +0530

    :zap: Add window.ethereum.enable() to popup metamask

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -2,9 +2,13 @@ import Web3 from "web3";
 let web3;
 
 if (typeof window !== "undefined") {
-  if (window.ethereum) {
-    web3 = new Web3(window.ethereum);
+  async function requestAccess() {
+    if (window.ethereum) {
+      web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
   }
+  requestAccess();
 } else {
   const provider = new Web3.providers.HttpProvider(
     "https://rinkeby.infura.io/v3/98667df842b643c5a68077377ac327a2"
